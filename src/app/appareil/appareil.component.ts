ce05f55532056a93694a3722032a1719ce958cb4
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-13 16:28:14 +0200

    Attribute directives ngStyle & ngClass

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -5,17 +5,26 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './appareil.component.html',
   styleUrls: ['./appareil.component.scss']
 })
-export class AppareilComponent implements OnInit{
-  @Input() appareilName: string ="";
+export class AppareilComponent implements OnInit {
+  @Input() appareilName: string = "";
   @Input() appareilStutus: string = "";
 
 
   constructor() {
   }
+
   ngOnInit(): void {
   }
+
   getStatus() {
     return this.appareilStutus;
   }
 
+  getColor(): any {
+    if (this.appareilStutus === "éteint") {
+      return 'red';
+    } else if (this.appareilStutus === "allumé") {
+      return 'green';
+    }
+  }
 }
