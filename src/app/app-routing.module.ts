35853eac0fcc2615b906d2cf51a1ad5529d0156c
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-13 11:29:21 +0200

    text interpolation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MonPremierComponent} from "./mon-premier/mon-premier.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'first', component: MonPremierComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
