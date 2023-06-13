49b6b1503843427058be38d0aeed137dd1b3c820
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-13 14:15:44 +0200

    Two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
@@ -14,6 +14,8 @@ import { AppareilComponent } from './appareil/appareil.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
