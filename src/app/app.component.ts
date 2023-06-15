93c0fb663a5933d36543e7d481b2f3cf39cc54a9
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-15 17:38:13 +0200

    observable set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, Observable} from "rxjs";
 
 
 @Component({
@@ -6,7 +7,23 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  secondes: number = 0;
 constructor() {
 }
+
+  ngOnInit(): void {
+  const counter = interval(1000);
+  counter.subscribe(
+    (value:number) => {
+      this.secondes = value;
+    },
+    (error :any) => {
+      console.log("Une erreur a été rencontrée");
+    },
+    () => {
+      console.log("Observable complétée");
+    }
+  )
+  }
 }
